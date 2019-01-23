17ded3da9a7f869c43be73e74ca07e8cc6cc8a10
xulzee <xulzee@163.com>
2019-01-23 22:32:15 +0800

    uniform

---
+++ 118.pascals-triangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        # if numRows == 0:
+        #     return []
+        output = []
+        for row_index in range(numRows):
+            current = [0] * (row_index + 1)
+            current[0] = 1
+            current[row_index] = 1
+            for column_index in range(1, row_index):
+                current[column_index] = output[row_index-1][column_index-1] + output[row_index-1][column_index]
+            output.append(current)
+        return output
+
+
+if __name__ == '__main__':
+    N = 0
+    print(Solution().generate(N))
