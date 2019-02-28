2ce80775ea6cb843dbc4618c31624fc41ee13da6
xulzee <xulzee@163.com>
2019-02-28 23:41:14 +0800

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -9,13 +9,22 @@ from typing import List
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            for i in s:
-                if i not in t:
+        s_list = [0] * 26
+        l_s = len(s)
+        l_t = len(t)
+        if l_s != l_t:
+            return False
+        else:
+            ord_a = ord('a')
+            for i in range(l_s):
+                s_list[ord(s[i]) - ord_a] += 1
+                s_list[ord(t[i]) - ord_a] -= 1
+            for j in s_list:
+                if j != 0:
                     return False
             return True
-        else:
-            return False
+
+
 
 if __name__ == '__main__':
     s = "anagram"
